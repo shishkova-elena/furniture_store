bc566a00a1d0f37e5eb2b6c2a17ef710404c880c
kudryasha1981 <105546279+kudryasha1981@users.noreply.github.com>
2024-01-10 18:38:23 +0300

    commit project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './320-767.css'
+import './768-1440.css'
+import './1440min.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
